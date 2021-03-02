fddf38a1c46a6177a15a597e62783ea90d22d14a
HappyBreaker <75553500+HappyBreaker@users.noreply.github.com>
2021-03-02 12:04:28 +0800

    0302 夏

---
+++ 2D_Reel_MainSpirit/Assets/程式/camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    [Header("追蹤物件")]
+    public Transform target;
+    [Header("追蹤速度"), Range(0f, 100f)]
+    public float speed = 1;
+
+    private void LateUpdate()
+    {
+        CameraTracker(); 
+    }
+
+    private void CameraTracker()
+    {
+        Vector3 pos1 = target.position;
+        Vector3 pos2 = transform.position;
+        pos1.z = -10;
+
+        pos2 = Vector3.Lerp(pos2, pos1, 0.5f * speed * Time.deltaTime);
+        transform.position = pos2;
+    }
+}
