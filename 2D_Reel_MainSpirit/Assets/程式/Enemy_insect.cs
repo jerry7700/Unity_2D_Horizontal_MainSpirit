563f4aad67e981534e61ff2f6f83e674168e7b9d
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-03-16 21:21:36 +0800

    怪物移動

---
+++ 2D_Reel_MainSpirit/Assets/程式/Enemy_insect.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class Enemy_insect : MonoBehaviour
+{
+    #region 欄位
+    [Header("左")]
+    public Transform leftPoint;
+    [Header("右")]
+    public Transform rightPoint;
+    [Header("速度"),Range(1,50)]
+    public float speed = 2.5f;
+
+    private float leftX, rightX;
+    private bool faceLeft = true;
+    private Rigidbody2D rb;
+    #endregion
+
+    #region 事件
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //斷絕父子關係
+        transform.DetachChildren();
+        //取得左右的X
+        leftX = leftPoint.position.x;
+        rightX = rightPoint.position.x;
+        //刪除左右
+        Destroy(leftPoint.gameObject);
+        Destroy(rightPoint.gameObject);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        //如果(面向左)
+        if(faceLeft)
+        {
+            //往左移動
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            if(transform.position.x < leftX)
+            {
+                //轉向
+                transform.localScale = new Vector3(1,1,1);
+                faceLeft = false;
+            }
+        }
+        else
+        {
+            //往右移動
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if (transform.position.x > rightX)
+            {
+                //轉向
+                transform.localScale = new Vector3(-1, 1, 1);
+                faceLeft = true;
+            }
+        }
+    }
+
+    #endregion
+}
