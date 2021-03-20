00b23669c67a0c6f5e33bd916101835f952f49c8
HappyBreaker <75553500+HappyBreaker@users.noreply.github.com>
2021-03-21 03:50:17 +0800

    0321

---
+++ 2D_Reel_MainSpirit/Assets/程式/trap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    private Transform tra;
+    private Health hp;
+    private Transform ob1;
+
+    private void Start()
+    {
+        hp = FindObjectOfType<Health>();
+        ob1 = GameObject.Find("主角").transform;
+        tra = GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hp.health--;
+        ob1.position = tra.position;
+    }
+
+}
